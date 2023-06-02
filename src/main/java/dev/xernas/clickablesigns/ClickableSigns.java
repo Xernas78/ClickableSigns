9dcadac43c963bd0e4c2771f404fc24b637479ef
Xernas Dev <71571183+Xernas78@users.noreply.github.com>
2023-06-02 21:27:18 +0200

    Add files via upload

---
+++ src/main/java/dev/xernas/clickablesigns/ClickableSigns.java
@@ -0,0 +1,17 @@
+package dev.xernas.clickablesigns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ClickableSigns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
